002207a8da3ff06a7491f673fff493d92ebc3c09
goziyyy <fariscraft709@gmail.com>
2025-02-13 12:27:45 +0700

    Update 13-02-2025 12:27:45: [Modified] api/admin/index_admin.php

--- api/admin/index_admin.php
+++ api/admin/index_admin.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard Admin | LaundryApp</title>
-    <link href="./css/bootstrap.min.css" rel="stylesheet">
-    <link href="./css/custom.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/custom.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
