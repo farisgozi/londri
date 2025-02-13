18d373bf81f218d62b36929449cbedbf2e743709
goziyyy <fariscraft709@gmail.com>
2025-02-13 07:38:58 +0700

    Update 13-02-2025 07:38:58: [Modified] api/kasir/proses_tambah_member.php

--- api/kasir/proses_tambah_member.php
+++ api/kasir/proses_tambah_member.php
@@ -1,29 +1,24 @@
 <?php
 if($_POST){
-    $id_member=$_POST['id_member'];
     $nama_member=$_POST['nama_member'];
     $alamat=$_POST['alamat'];
     $jenis_kelamin=$_POST['jenis_kelamin'];
     $tlp=$_POST['tlp'];
 
-if(empty($id_member)){
-    echo "<script>alert('Id member tidak boleh kosong');location.href='member.php';</script>";
-    } elseif(empty($nama_member)){
-    echo "<script>alert('nama member tidak boleh kosong');location.href='member.php';</script>";
+    if(empty($nama_member)){
+        echo "<script>alert('Nama member tidak boleh kosong');location.href='member.php';</script>";
     } elseif(empty($tlp)){
-    echo "<script>alert('No Telp tidak boleh kosong');location.href='member.php';</script>";
+        echo "<script>alert('No Telp tidak boleh kosong');location.href='member.php';</script>";
     } else {
-    include "koneksi.php";
-    $insert=mysqli_query($conn,"insert into member
-    (id_member, nama_member, alamat, jenis_kelamin, tlp) 
-    value
-    ('".$id_member."','".$nama_member."','".$alamat."','".$jenis_kelamin."','".$tlp."')") or
-    die(mysqli_error($conn));
-if($insert){
-    echo "<script>alert('Sukses menambahkan member');location.href='member.php';</script>";
-} else {
-    echo "<script>alert('Gagal menambahkan member');location.href='member.php';</script>";
-}
-}
+        include "koneksi.php";
+        $insert=mysqli_query($conn,"INSERT INTO member (nama_member, alamat, jenis_kelamin, tlp) 
+        VALUES ('".$nama_member."','".$alamat."','".$jenis_kelamin."','".$tlp."')") or
+        die(mysqli_error($conn));
+        if($insert){
+            echo "<script>alert('Sukses menambahkan member');location.href='member.php';</script>";
+        } else {
+            echo "<script>alert('Gagal menambahkan member');location.href='member.php';</script>";
+        }
+    }
 }
 ?>
