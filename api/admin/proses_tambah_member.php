c8e08fca5d4cec469926a1709ee5c614140f8e4e
goziyyy <fariscraft709@gmail.com>
2025-02-13 20:36:48 +0700

    Update 13-02-2025 20:36:48: [Modified] api/admin/proses_tambah_member.php

--- api/admin/proses_tambah_member.php
+++ api/admin/proses_tambah_member.php
@@ -7,29 +7,29 @@ if($_POST){
 
     // Validate jenis_kelamin enum
     if(!in_array($jenis_kelamin, ['Laki-laki', 'Perempuan'])) {
-        echo "<script>alert('Jenis kelamin harus Laki-laki atau Perempuan');location.href='./member.php';</script>";
+        echo "<script>alert('Jenis kelamin harus Laki-laki atau Perempuan');location.href='member.php';</script>";
         exit;
     }
 
     // Validate phone number length
     if(strlen($tlp) > 15) {
-        echo "<script>alert('Nomor telepon maksimal 15 digit');location.href='./member.php';</script>";
+        echo "<script>alert('Nomor telepon maksimal 15 digit');location.href='member.php';</script>";
         exit;
     }
 
     if(empty($nama_member)){
-        echo "<script>alert('Nama member tidak boleh kosong');location.href='./member.php';</script>";
+        echo "<script>alert('Nama member tidak boleh kosong');location.href='member.php';</script>";
     } elseif(empty($tlp)){
-        echo "<script>alert('No Telp tidak boleh kosong');location.href='./member.php';</script>";
+        echo "<script>alert('No Telp tidak boleh kosong');location.href='member.php';</script>";
     } else {
         include "koneksi.php";
         $insert = mysqli_query($conn, "INSERT INTO member (nama_member, alamat, jenis_kelamin, tlp) 
         VALUES ('$nama_member','$alamat','$jenis_kelamin','$tlp')") or
         die(mysqli_error($conn));
         if($insert){
-            echo "<script>alert('Sukses menambahkan member');location.href='./member.php';</script>";
+            echo "<script>alert('Sukses menambahkan member');location.href='member.php';</script>";
         } else {
-            echo "<script>alert('Gagal menambahkan member');location.href='./member.php';</script>";
+            echo "<script>alert('Gagal menambahkan member');location.href='member.php';</script>";
         }
     }
 }
