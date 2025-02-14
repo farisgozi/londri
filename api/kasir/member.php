59f3642d5212eb810090769a23dc9c041c6bfb02
goziyyy <fariscraft709@gmail.com>
2025-02-14 13:53:40 +0700

    Update 14-02-2025 13:53:40: [Modified] api/kasir/index_kasir.php [Modified] api/kasir/member.php

--- api/kasir/member.php
+++ api/kasir/member.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pelanggan | LaundryApp</title>
+    <title>Pelanggan | Laundries</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/custom.css" rel="stylesheet">
     <link href="../assets/plugins/fontawesome/css/all.min.css" rel="stylesheet">
