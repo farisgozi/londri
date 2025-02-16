d65be5752f99ce70364ed4d6d0c6f0ebb2d873d3
goziyyy <fariscraft709@gmail.com>
2025-02-17 06:46:19 +0700

    Update 17-02-2025 06:46:19: [Modified] api/admin/detail_transaksi.php [Modified] api/admin/index_admin.php

--- api/admin/index_admin.php
+++ api/admin/index_admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard Admin | Launners</title>
+    <title>Dashboard Admin | Launnres</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/custom.css" rel="stylesheet">
     <link href="../assets/plugins/fontawesome/css/all.min.css" rel="stylesheet">
