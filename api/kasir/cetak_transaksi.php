5a58cb5fafa321d1d9f64a61a9964531e96b2233
goziyyy <fariscraft709@gmail.com>
2025-02-17 06:53:45 +0700

    Update 17-02-2025 06:53:45: [Modified] api/kasir/cetak_transaksi.php

--- api/kasir/cetak_transaksi.php
+++ api/kasir/cetak_transaksi.php
@@ -232,7 +232,7 @@
             <li>Bukti tagihan yang sah di dalam transaksi kami adalah melalui nota digital atau nota fisik.</li>
             <li>Kami tidak bertanggung jawab apabila terjadi kehilangan / kerusakan pada laundry bersih yang tidak diambil selama 1 bulan di workshop kami.</li>
             <li>Kami menggunakan bahan detergent dan parfum dengan standar laundry tanpa mengetahui jenis alergi yang anda miliki. Segala jenis penggunaan sabun atau parfum milik pribadi tidak diperkenankan untuk digunakan di laundry kami dan apabila terjadi reaksi alergi pada kulit bukan menjadi tanggung jawab Launnres Laundry.</li>
-            <li>Pengaduan harap disampaikan secara tertulis di ke email familylaundry@gmail.com dengan melampirkan nomor nota, video unboxing serta keluhan yang dialami.</li>
+            <li>Pengaduan harap disampaikan secara tertulis di ke email launnreslaundry@gmail.com dengan melampirkan nomor nota, video unboxing serta keluhan yang dialami.</li>
         </ol>
     </div>
 
