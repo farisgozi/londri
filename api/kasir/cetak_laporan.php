b535392bf1b004acdfe49ee2e4958257924ebfae
goziyyy <fariscraft709@gmail.com>
2025-02-17 06:48:12 +0700

    Update 17-02-2025 06:48:12: [Modified] api/kasir/cetak_laporan.php [Modified] api/kasir/cetak_transaksi.php

--- api/kasir/cetak_laporan.php
+++ api/kasir/cetak_laporan.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Cetak Laporan Transaksi | Launners</title>
+    <title>Cetak Laporan Transaksi | Launnres</title>
     <style type="text/css">
         body {
             font-family: Arial, sans-serif;
