a55819f43c47e85982d6555884982c230fc473a1
goziyyy <fariscraft709@gmail.com>
2025-02-16 20:40:29 +0700

    Update 16-02-2025 20:40:29: [Modified] api/admin/proses_tambah_member.php

--- api/admin/proses_tambah_member.php
+++ api/admin/proses_tambah_member.php
@@ -1,25 +1,9 @@
 <?php
 if($_POST){
-    $nama_member = $_POST['nama_member'];
-    $alamat = $_POST['alamat'];
-    $jenis_kelamin = $_POST['jenis_kelamin'];
-    $tlp = $_POST['tlp'];
-
-    // Map L/P to full text as per database enum
-    if($jenis_kelamin == 'L') {
-        $jenis_kelamin = 'Laki-laki';
-    } elseif($jenis_kelamin == 'P') {
-        $jenis_kelamin = 'Perempuan';
-    } else {
-        echo "<script>alert('Jenis kelamin tidak valid');location.href='member.php';</script>";
-        exit;
-    }
-
-    // Validate phone number length
-    if(strlen($tlp) > 15) {
-        echo "<script>alert('Nomor telepon maksimal 15 digit');location.href='member.php';</script>";
-        exit;
-    }
+    $nama_member=$_POST['nama_member'];
+    $alamat=$_POST['alamat'];
+    $jenis_kelamin=$_POST['jenis_kelamin'];
+    $tlp=$_POST['tlp'];
 
     if(empty($nama_member)){
         echo "<script>alert('Nama member tidak boleh kosong');location.href='member.php';</script>";
@@ -27,8 +11,8 @@ if($_POST){
         echo "<script>alert('No Telp tidak boleh kosong');location.href='member.php';</script>";
     } else {
         include "koneksi.php";
-        $insert = mysqli_query($conn, "INSERT INTO member (nama_member, alamat, jenis_kelamin, tlp) 
-        VALUES ('$nama_member','$alamat','$jenis_kelamin','$tlp')") or
+        $insert=mysqli_query($conn,"INSERT INTO member (nama_member, alamat, jenis_kelamin, tlp) 
+        VALUES ('".$nama_member."','".$alamat."','".$jenis_kelamin."','".$tlp."')") or
         die(mysqli_error($conn));
         if($insert){
             echo "<script>alert('Sukses menambahkan member');location.href='member.php';</script>";
